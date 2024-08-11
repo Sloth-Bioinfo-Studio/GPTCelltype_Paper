9c06a751c9790ff5a0e24395117841def4c0697c
Silen Wang <qiumin14@163.com>
2024-08-12 00:39:18 +0800

    部分解读

--- anno/cl/generate.R
+++ anno/cl/generate.R
@@ -1,15 +1,15 @@
 library(rols)
 
-d <- read.csv('Dropbox/research/gptcelltype/anno/compiled/gpt4topgenenumber.csv',as.is=T,na.strings = NULL)
-d2 <- read.csv('Dropbox/research/gptcelltype/anno/compiled/all.csv',as.is=T,na.strings = NULL)
+d <- read.csv('../anno/compiled/gpt4topgenenumber.csv',as.is=T,na.strings = NULL)
+d2 <- read.csv('../anno/compiled/all.csv',as.is=T,na.strings = NULL)
 
-d3 <- read.csv('Dropbox/research/gptcelltype/simu/gpt4aug3/noise.csv',as.is=T,na.strings = NULL)
-d4 <- read.csv('Dropbox/research/gptcelltype/simu/gpt4aug3/subset.csv',as.is=T,na.strings = NULL)
+d3 <- read.csv('../simu/gpt4aug3/noise.csv',as.is=T,na.strings = NULL)
+d4 <- read.csv('../simu/gpt4aug3/subset.csv',as.is=T,na.strings = NULL)
 
 ctlist <- c(unlist(d[,grep('annotation',colnames(d))]),unlist(d2[,grep('annotation',colnames(d2))]),d3$celltype,d3$annotation,d4$celltype,d4$annotation)
 ct <- sort(unique(sub('cells','cell',tolower(ctlist))))
 
-ex <- read.csv('Dropbox/research/gptcelltype/anno/cl/compiled.csv',as.is=T,header=T)
+ex <- read.csv('../anno/cl/compiled.csv',as.is=T,header=T)
 
 ct <- ct[!ct%in%ex[,1]]
 cv <- sapply(ct,function(i) {
@@ -25,4 +25,4 @@ cv <- data.frame(cv,type=NA)
 colnames(cv) <- colnames(ex)
 cv <- rbind(cv,ex)
 
-write.csv(cv,file='Dropbox/research/gptcelltype/anno/cl/compiled.csv',row.names = F)
+write.csv(cv,file='../anno/cl/compiled.csv',row.names = F)
