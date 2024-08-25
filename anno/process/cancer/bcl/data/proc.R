e021bdac12aca9bbca22fa6d4c5a676d427a1885
Sylens Wong <qium@aimingmed.com>
2024-08-25 16:22:48 +0800

    bcl process script mod

--- anno/process/cancer/bcl/data/proc.R
+++ anno/process/cancer/bcl/data/proc.R
@@ -1,6 +1,8 @@
 library(anndata)
 library(Matrix)
-d <- anndata::read_h5ad('/celltype/bcl/data/raw/scRNA_BCR_TCR.h5ad')
+# d <- anndata::read_h5ad('/celltype/bcl/data/raw/scRNA_BCR_TCR.h5ad')
+d <- anndata::read_h5ad(snakemake@input[['h5ad']])
+
 ct <- oct <- as.character(d$obs$refined_clusters)
 ct[grep('B[0-9]*',oct)] <- 'Malignant B cells'
 ct[oct=='B0'] <- 'Non-malignant B cells'
@@ -9,6 +11,11 @@ ct[grep('CD8 T',oct)] <- 'CD8 T cells'
 names(ct) <- rownames(d$obs)
 m <- t(d$X)
 n <- t(log2(d$X/rowSums(d$X)*10000+1))
-saveRDS(ct,file='/celltype/bcl/data/proc/ct.rds')
-saveRDS(m,file='/celltype/bcl/data/proc/count.rds')
-saveRDS(n,file='/celltype/bcl/data/proc/norm.rds')
+
+# saveRDS(ct,file='/celltype/bcl/data/proc/ct.rds')
+# saveRDS(m,file='/celltype/bcl/data/proc/count.rds')
+# saveRDS(n,file='/celltype/bcl/data/proc/norm.rds')
+
+saveRDS(m,file=snakemake@output[['count_rds']])
+saveRDS(ct,file=snakemake@output[['anno']])
+saveRDS(n,file=snakemake@output[['norm']])
