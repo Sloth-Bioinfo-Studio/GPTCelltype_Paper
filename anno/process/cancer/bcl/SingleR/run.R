6a7afcca1fc89ee3416685d0355b5a782c7d52ee
Sylens Wong <qium@aimingmed.com>
2024-09-02 15:16:42 +0800

    bcl done

--- anno/process/cancer/bcl/SingleR/run.R
+++ anno/process/cancer/bcl/SingleR/run.R
@@ -1,16 +1,26 @@
 suppressMessages(library(celldex))
 hpca.se <- HumanPrimaryCellAtlasData()
 suppressMessages(library(SingleR))
-clu <- readRDS('/celltype/cancer/bcl/data/proc/ct.rds')
-d <- readRDS('/celltype/cancer/bcl/data/proc/norm.rds')
+# clu <- readRDS('/celltype/cancer/bcl/data/proc/ct.rds')
+# d <- readRDS('/celltype/cancer/bcl/data/proc/norm.rds')
+
+dir.create(snakemake@params[['path']])
+clu <- readRDS(snakemake@input[['anno']])
+d <- readRDS(snakemake@input[['norm']])
+
 rt <- system.time({pred <- SingleR(test = d, ref = hpca.se, assay.type.test=1,labels = hpca.se$label.main)})
-saveRDS(pred,file='/celltype/cancer/bcl/SingleR/fullpred.rds')
-saveRDS(rt,file='/celltype/cancer/bcl/SingleR/time.rds')
+
+# saveRDS(pred,file='/celltype/cancer/bcl/SingleR/fullpred.rds')
+# saveRDS(rt,file='/celltype/cancer/bcl/SingleR/time.rds')
+saveRDS(pred,file=snakemake@output[['fullpred']])
+saveRDS(rt,file=snakemake@output[['time']])
+
 mat <- pred$scores
 rownames(mat) <- pred@rownames
 k <- rowsum(mat[names(clu),],clu)
 predct <- colnames(k)[apply(k,1,which.max)]
 orict <- rownames(k)
 ct <- data.frame(orict=orict,predct=predct)
-saveRDS(ct,file='/celltype/cancer/bcl/SingleR/res.rds')
 
+# saveRDS(ct,file='/celltype/cancer/bcl/SingleR/res.rds')
+saveRDS(ct,file=snakemake@output[['res']])
